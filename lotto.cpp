1112bfef5b15197bca483f43425e38463f523966
Adam Torok <edems96@gmail.com>
2015-05-18 22:41:13 +0200

    #2

--- lotto.cpp
+++ lotto.cpp
@@ -1,13 +1,73 @@
 #include "lotto.h"
 
-void main(int argc, char** args) {
+int main(int argc, char** args) {
 	
 	if( !readFile("otos.csv") )
 		return 0;
 	
 	printf("Lottok: %d\n", lottok.size());
 	
+	elozo();
+	//stat1();
+	stat2();
+	
 	return 1;
+}
+
+void elozo() {
+	uint c = 0;
+	
+	for(uint i = 0; i < lottok.size()-1; i++) {
+		if( in_array(lottok[i].szamok, 5, lottok[i+1].szamok, 5) )
+			c++;
+	}
+	
+	printf("Elozo: %d/%d = %.3f\n", c, lottok.size(), ((float)c / (float)lottok.size()) * 100.0f);
+}
+
+void stat1() {
+	uint c[90] = {0};
+	
+	for(uint i = 0; i < lottok.size(); i++) {
+		for(uint j = 0; j < 5; j++) {
+			c[lottok[i].szamok[j]-1]++;
+		}
+	}
+	
+	uint ossz = lottok.size() * 5;
+	for(uint i = 0; i < 90; i++) {
+		printf("%d: %d / %d (%.2f)\n", i+1, c[i], ossz, ((float)c[i] / (float)ossz) * 100.f);
+	}
+	
+}
+
+void stat2() {
+	uint c[90] = {0};
+	
+	for(uint i = 0; i < lottok.size(); i++) {
+		for(uint j = 0; j < 5; j++) {
+			c[lottok[i].szamok[j]-1]++;
+		}
+	}
+	
+	uint max = 0;
+	for(uint i = 0; i < 90; i++) {
+		if( c[i] > max )
+			max = c[i];
+	}
+	
+	uint ossz = lottok.size() * 5;
+	
+	for(uint j = max; j >= 0; j--) {
+		for(uint i = 0; i < 90; i++) {
+			if( c[i] != j )
+				continue;
+			
+			printf("%d: %d / %d (%.2f)\n", i+1, c[i], ossz, ((float)c[i] / (float)ossz) * 100.f);
+		}
+	}
+	
+	
 }
 
 bool readFile(const char* filename) {
@@ -19,17 +79,31 @@ bool readFile(const char* filename) {
 		return false;
 	}
 	
+	printf("Read file: %s\n", filename);
+	
 	int r;
 	while( true ) {
 		Lotto l;
 		
 		r = fscanf(f, 
-			"%d;%d;%d.%d.%d.;%d;%d Ft;%d;%d Ft;%d;%d Ft;%d;%d Ft;%d;%d;%d;%d;%d\n",
+			"%d;%d;",
 			&l.ev,
-			&l.het,
-			&l.datum.ev,
-			&l.datum.honap,
-			&l.datum.nap,
+			&l.het);
+			
+		if( r != 2 )
+			break;
+
+		r = fscanf(f, 
+			"%d.%d.%d.;",
+			&l.huzas.ev,
+			&l.huzas.honap,
+			&l.huzas.nap);
+			
+		if( r != 3 )
+			fgetc(f); // ;
+		
+		r = fscanf(f,
+			"%d;%dFt;%d;%dFt;%d;%dFt;%d;%dFt;%d;%d;%d;%d;%d\r\n",
 			&l.talalat_db_5,
 			&l.talalat_ft_5,
 			&l.talalat_db_4,
@@ -44,7 +118,7 @@ bool readFile(const char* filename) {
 			&l.szamok[3],
 			&l.szamok[4]);
 			
-		if( r == EOF )
+		if( r != 13 )
 			break;
 		
 		lottok.push_back(l);
@@ -53,4 +127,22 @@ bool readFile(const char* filename) {
 	fclose(f);
 	
 	return true;
+}
+
+bool in_array(uint number, uint *array, uint size) {
+	for(uint i = 0; i < size; i++) {
+		if( array[i] == number )
+			return true;
+	}
+	
+	return false;
+}
+
+bool in_array(uint *a1, uint s1, uint *a2, uint s2) {
+	for(uint i = 0; i < s1; i++) {
+		if( in_array(a1[i], a2, s2) )
+			return true;
+	}
+	
+	return false;
 }
--- lotto.h
+++ lotto.h
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include <vector>
 
+using namespace std;
+
 typedef unsigned int uint;
+typedef unsigned long ulong;
 
 struct Datum {
 	uint ev, honap, nap;
@@ -11,17 +14,17 @@ struct Lotto {
 	uint ev, het;
 	Datum huzas;
 	
-	uint talalat_db_5;
-	uint talalat_ft_5;
+	ulong talalat_db_5;
+	ulong talalat_ft_5;
 	
-	uint talalat_db_4;
-	uint talalat_ft_4;
+	ulong talalat_db_4;
+	ulong talalat_ft_4;
 	
-	uint talalat_db_3;
-	uint talalat_ft_3;
+	ulong talalat_db_3;
+	ulong talalat_ft_3;
 	
-	uint talalat_db_2;
-	uint talalat_ft_2;
+	ulong talalat_db_2;
+	ulong talalat_ft_2;
 	
 	uint szamok[5];
 	
@@ -29,8 +32,8 @@ struct Lotto {
 		printf("#LOTTO#\n");
 		printf("\tEv: %d\n", ev);
 		printf("\tHet: %d\n", het);
-		printf("\tHuzas: %d. %d. %d\n", datum.ev, datum.honap, datum.nap);
-		printf("\tTalalat:\n")
+		printf("\tHuzas: %d. %d. %d\n", huzas.ev, huzas.honap, huzas.nap);
+		printf("\tTalalat:\n");
 		printf("\t\t5 (db): %d\n", talalat_db_5);
 		printf("\t\t5 (Ft): %d\n", talalat_ft_5);
 		printf("\t\t4 (db): %d\n", talalat_db_4);
@@ -39,12 +42,19 @@ struct Lotto {
 		printf("\t\t3 (Ft): %d\n", talalat_ft_3);
 		printf("\t\t2 (db): %d\n", talalat_db_2);
 		printf("\t\t2 (Ft): %d\n", talalat_ft_2);
-		printf("\tSzamok: %d, %d, %d, %d, %d", szamok[0], szamok[1], szamok[2], szamok[3], szamok[4]);
+		printf("\tSzamok: %d, %d, %d, %d, %d\n", szamok[0], szamok[1], szamok[2], szamok[3], szamok[4]);
 		printf("#######\n\n");
 	}
 };
 
 vector<Lotto> lottok;
 
-void main(int argc, char** args);
+int main(int argc, char** args);
 bool readFile(const char* filename);
+
+void elozo();
+void stat1();
+void stat2();
+
+bool in_array(uint number, uint *array, uint size);
+bool in_array(uint *a1, uint s1, uint *a2, uint s2);
